22a5b2705f3d5effd84858e2c05a6e21d45b1621
Icarus-An <zhiyuan.an@foxmail.com>
2020-03-13 01:18:30 +0800

    0.0.1

---
+++ scripts/main.js
@@ -0,0 +1,35 @@
+
+let myImage = document.querySelector('img');
+
+myImage.onclick = function() {
+    let mySrc = myImage.getAttribute('src');
+    if(mySrc === 'images/passport.jpg') {
+      myImage.setAttribute('src', 'images/Stalin.jpg');
+    } else {
+      myImage.setAttribute('src', 'images/passport.jpg');
+    }
+}
+
+let myButton = document.querySelector('button');
+let myHeading = document.querySelector('h1');
+
+function setUserName() {
+  let myName = prompt('请输入你的名字。');
+  if(!myName || myName === null){
+    setUserName();
+  }else{
+    localStorage.setItem('name', myName);
+    myHeading.textContent = 'Mozilla 酷毙了，' + myName;
+  }
+}
+
+if(!localStorage.getItem('name')) {
+  setUserName();
+} else {
+  //let storedName = localStorage.getItem('name');
+  myHeading.textContent = 'Mozilla is COOL，' + localStorage.getItem('name');
+}
+
+myButton.onclick = function() {
+  setUserName();
+}
